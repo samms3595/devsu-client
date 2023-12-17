ba6d2862109c225646fc6b6c8de992b71ceed176
Sebastian Mendez <semendez@gbm.net>
2023-12-16 20:47:29 -0500

    Implementando Swagger Api

--- src/main/java/com/devsu/clientes/ClientesApplication.java
+++ src/main/java/com/devsu/clientes/ClientesApplication.java
@@ -1,7 +1,11 @@
 package com.devsu.clientes;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
@@ -10,4 +14,13 @@ public class ClientesApplication {
         SpringApplication.run(ClientesApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestion de Clientes")
+                        .version("1.0.0")
+                        .description("Este es un proyecto creado para la prueba de Devsu")
+                        .license(new License().name("Sebastian Mendez")));
+    }
 }
--- src/main/java/com/devsu/clientes/controller/ClienteController.java
+++ src/main/java/com/devsu/clientes/controller/ClienteController.java
@@ -1,9 +1,16 @@
 package com.devsu.clientes.controller;
 
+import com.devsu.clientes.model.ApiError;
 import com.devsu.clientes.model.Cliente;
 import com.devsu.clientes.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +21,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/cliente")
+@Tag(name = "Cliente", description = "API para operaciones relacionadas con los clientes")
 public class ClienteController {
 
     private final ClienteService clienteService;
@@ -22,36 +30,60 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    @Operation(summary = "Obtiene una lista de todos los clientes", responses = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta / Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
     @GetMapping
     public ResponseEntity<List<Cliente>> obtenerClientes() {
         List<Cliente> clientes = clienteService.obtenerTodosClientes();
         return ResponseEntity.ok(clientes);
     }
 
+    @Operation(summary = "Obtiene un cliente por su ID", responses = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))),
+            @ApiResponse(responseCode = "400", description = "Cliente no encontrado / Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> obtenerClientePorId(@PathVariable Long id) {
         Cliente cliente = clienteService.obtenerClientePorId(id);
         return ResponseEntity.ok(cliente);
     }
 
+    @Operation(summary = "Obtiene un cliente por su ID de cliente", responses = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))),
+            @ApiResponse(responseCode = "400", description = "Cliente no encontrado / Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
     @GetMapping("/client/{id}")
     public ResponseEntity<Cliente> obtenerClientePorClienteId(@PathVariable Long id) {
         Cliente cliente = clienteService.obtenerClientePorClienteId(id);
         return ResponseEntity.ok(cliente);
     }
 
-    @PostMapping
+    @Operation(summary = "Guarda un nuevo cliente", responses = {
+            @ApiResponse(responseCode = "201", description = "Cliente creado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta / Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Cliente> guardarCliente(@Valid @RequestBody Cliente cliente) {
         Cliente clienteGuardado = clienteService.guardarCliente(cliente);
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteGuardado);
     }
 
-    @PutMapping
+    @Operation(summary = "Actualiza un cliente existente", responses = {
+            @ApiResponse(responseCode = "200", description = "Cliente actualizado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta / Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Cliente> actualizarCliente(@Valid @RequestBody Cliente cliente) {
         Cliente clienteActualizado = clienteService.actualizarCliente(cliente);
         return ResponseEntity.ok(clienteActualizado);
     }
 
+    @Operation(summary = "Elimina un cliente por su ID", responses = {
+            @ApiResponse(responseCode = "200", description = "Cliente eliminado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class))),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta / Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> borrarCliente(@PathVariable Long id) {
         clienteService.eliminarCliente(id);
