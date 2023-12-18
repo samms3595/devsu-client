0132c9fa7174d66b9ec9c3008a7479bebd40b17d
Sebastian Mendez <semendez@gbm.net>
2023-12-17 20:06:23 -0500

    Añadiendo Script de bd y Postman

---
+++ CreateDbClientes.sql
@@ -0,0 +1,18 @@
+create database clientes;
+use clientes;
+create table cliente_cuentas (cliente_id bigint not null, cuentas bigint) engine=InnoDB;
+create table persona (dtype varchar(31) not null,
+                      id bigint not null,
+                      apellidos varchar(255),
+                      direccion varchar(255),
+                      edad integer not null check ((edad>=1) and (edad<=150)),
+                      genero enum ('FEMENINO','MASCULINO','NO_ESPECIFICA') not null,
+                      identificacion varchar(255),
+                      nombre varchar(255),
+                      telefono varchar(255),
+                      client_id bigint,
+                      estado tinyint check (estado between 0 and 1),
+                      password varchar(255), primary key (id)) engine=InnoDB;
+CREATE USER 'clientes'@'localhost' IDENTIFIED BY 'Devsu123.';
+GRANT ALL PRIVILEGES ON clientes TO 'clientes'@'localhost';
+FLUSH PRIVILEGES;
